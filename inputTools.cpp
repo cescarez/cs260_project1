6c7553b6293e52d97d8c3fde4553b43a2209a7ff
Christabel Escarez <christabel.escarez@gmail.com>
2019-04-14 00:22:22 -0700

    populated input tools implementation and header files

--- inputTools.cpp
+++ inputTools.cpp
@@ -4,3 +4,131 @@
 //implementation file for standard input tools
 
 #include "inputTools.h"
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+char checkInput (char &response) {
+	int responseCount = 0;
+	//check for valid input, reobtain user input
+	while (response != 'n' && response != 'y') {
+		//check for valid char input 
+		while (!response) {
+			//clear error from input stream
+			cin.clear();
+			//ignore the rest of the input that triggered the istream error
+			cin.ignore(100, '\n');
+			cout << "Invalid input. Please respond with either 'y' or 'n': " << endl;
+			//case insensitive response
+			response = tolower(response);	
+		}
+		//check for 'y' or 'n' response, with up to three attempts for valid input
+		if(responseCount < MAX_RESPONSE) {
+			cout << "Invalid input. Please respond with either 'y' or 'n': " << endl;
+			cin >> response;
+			response = tolower(response);	
+			responseCount++;
+		} else {
+			cout << "Error. Too many invalid inputs. Exiting MusicBox." << endl;
+			exit(1);
+		}
+	}
+	//ignore the newline from user input
+	cin.ignore (100, '\n');
+	//return char to calling environment
+	return response;
+}
+
+
+int checkInput(int &response, const int &maxThreshold) {
+	int responseCount = 0;
+	//check for valid input, reobtain user input
+	while (!response || response < 0 || response > maxThreshold) {
+		//check for non-integer inputs
+		while (!response) {
+			//clear error from input stream
+			cin.clear();
+			//ignore the rest of the input that triggered the istream error
+			cin.ignore(100, '\n');
+			cout << "Invalid input. Please enter a number between 0 and " << maxThreshold
+				 << ": " << endl;
+			cin >> response;
+		}
+		//check for integer is a positive real number and below the maxThreshold parameter,
+		//with up to three attempts for valid input
+		if(responseCount < MAX_RESPONSE) {
+			cout << "Invalid input. Please enter a number between 0 and " << maxThreshold
+				 << ": " << endl;
+			cin >> response;
+			responseCount++;
+		} else {
+			//exit program when invalid inputs > MAX_RESPONSE
+			cout << "Error. Too many invalid inputs. Exiting MusicBox." << endl;
+			exit(1);
+		}
+	}
+	//ignore the newline from user input
+	cin.ignore (100, '\n');
+	//return int to calling environment
+	return response;
+}
+
+bool checkCString(char response[]) {
+	int responseCount = 0;
+	//check for valid input stream
+	while (!response) {
+		//clear error from input stream
+		cin.clear();
+		//ignore the rest of the input that triggered the istream error
+		cin.ignore(100, '\n');
+		//re-obtain response from user, with up to three attempts for valid input
+		if(responseCount < MAX_RESPONSE){
+			cout << "Invalid input. Please re-enter response: " << endl;
+			cin >> response;
+			responseCount++;
+		} else {
+			//exit program when invalid inputs > MAX_RESPONSE
+			cout << "Error. Too many invalid inputs. Exiting MusicBox." << endl;
+			exit(1);
+		}
+	}	
+	//ignore the newline from user input
+	cin.ignore (100, '\n');
+	//return bool to calling environment
+	if (!response) 
+		return false;
+	else
+		return true;
+}
+
+int readInt(const char prompt[],  const int &maxThreshold)
+{
+	int	response;
+	int responseCount;
+
+	//prompt user for input
+	cout << endl << prompt;
+	cin >> response;
+
+	//check if a valid integer is entered
+	response = checkInput(response, maxThreshold);
+	//return int to calling environment 
+	return response;
+}
+
+char readString (const char prompt[], char response[])
+{
+	cout << endl << prompt;
+
+	cin.get(response, MAX_SIZE, '\n');
+	if(!checkCString(response)) {
+		cout << "Error. Input failure. Exiting MusicBox." << endl;
+	}
+	//no value returned in this function; function modifies parameter, 
+	//char response[], in calling environment
+
+}
+
+
+
--- inputTools.h
+++ inputTools.h
@@ -8,7 +8,16 @@
 
 
 const int MAX_SIZE = 100;
+const int MAX_RESPONSE = 3;
 
-
+//check for legal inputs; note overloaded checkInput
+char checkInput (char &response);
+int checkInput (int &response, const int &maxThreshold);
+bool checkCstring(char response[]);
+//display prompt for user input, returning the legal
+//value back to the calling environment
+int readInt (const char prompt[], const int &maxThreshold);
+char readChar (const char prompt[]);
+char readString (const char prompt[], char inputString[]);
 
 #endif
